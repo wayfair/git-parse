8f9a451a9d953d4be0813bd4288f14eddcffbb98
sorsini <sorsini@wayfair.com>
2021-09-29 22:35:26 -0400

    Update git log stream spec

--- src/__tests__/git_log_stream_spec.js
+++ src/__tests__/git_log_stream_spec.js
@@ -1,7 +1,8 @@
 import logStream from "../git_log_stream";
 
 describe("git log stream", () => {
-  fit("should throw an error if the directory does not exist", (done) => {
+  it("should throw an error if the directory does not exist", (done) => {
+    // function does not currently return a promise
     logStream("/fake_nonexistent_directory").addErrorHandler(() => done());
   });
 });
