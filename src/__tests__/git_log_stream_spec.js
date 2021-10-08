893226884cd13d2ecc20b03a7910f565517e0711
sorsini <sorsini@wayfair.com>
2021-10-07 20:34:37 -0400

    Upgrade specs

--- src/__tests__/git_log_stream_spec.js
+++ src/__tests__/git_log_stream_spec.js
@@ -2,8 +2,7 @@ import logStream from "../git_log_stream";
 
 describe("git log stream", () => {
   it("should throw an error if the directory does not exist", (done) => {
-    return logStream("/fake_nonexistent_directory").addErrorHandler(() =>
-      done()
-    );
+    // function does not currently return a promise
+    logStream("/fake_nonexistent_directory").addErrorHandler(() => done());
   });
 });
