cfcbb2b41051a47a33b3c4faaee540236803ba3e
kv979w <kvirtaneva@wayfair.com>
2021-11-22 10:25:03 -0500

    chore(eslint): configure typescript eslint

--- src/util.ts
+++ src/util.ts
@@ -4,9 +4,9 @@ import { join } from "path";
 export const resolveHome = (filepath: string) => {
   if (filepath[0] === "~") {
     const restOfPath: string = filepath.slice(1);
-    if (!process.env.HOME)
+    if (typeof process.env.HOME !== 'string')
       throw new Error("HOME environment variable is missing");
-    const home: string = process.env.HOME as string;
+    const home = process.env.HOME;
     return join(home, restOfPath);
   }
   return filepath;
